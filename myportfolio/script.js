610475abe7fa9ed0c62ed6a36a43c0750d304d00
samikshyadash <102473562+samikshyadash@users.noreply.github.com>
2022-03-28 00:10:09 +0530

    Add files via upload

---
+++ myportfolio/script.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+    const menuIcon = document.getElementById('btn')
+    const nav = document.querySelector('.menu-items')
+
+    menuIcon.addEventListener('click', () => {
+        nav.classList.toggle('active-nav');
+    })
+
+}
+
+navSlide();
